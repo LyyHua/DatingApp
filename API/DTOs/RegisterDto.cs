eefba661c5d974b27298f32a19f51049cc98957a
LyyHua <lyhuavanly@gmail.com>
2024-08-26 15:33:49 +0700

    Yes i am speedrunning this shit

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,5 +8,6 @@ public class RegisterDto
     [Required]
     public required string UserName { get; set; }
     [Required]
+    [StringLength(8, MinimumLength = 4)]
     public required string Password { get; set; }
 }
